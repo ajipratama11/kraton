02556cef035dedc0447cd0158f629728ca146a49
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2020-09-02 22:41:08 +0700

    template

---
+++ assets/js/custom.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+	var baseUrl = $("#baseUrl").data("url");
+	$(".datatable").DataTable();
+	$(".select2").select2();
+	$(".datepicker").datepicker({
+		format: "yyyy/mm/dd"
+	});
+	$("button[type='reset']").click(function() {
+		$(".select2")
+			.val(null)
+			.trigger("change");
+	});
+	$(".custom-file-input").on("change", function() {
+		var fileName = $(this)
+			.val()
+			.split("\\")
+			.pop();
+		$(this)
+			.siblings(".custom-file-label")
+			.addClass("selected")
+			.html(fileName);
+	});
+
+	$(".submitConfirm").submit(function(e) {
+		var id = $(this).attr("id");
+		e.preventDefault();
+		swal(
+			{
+				title: "Validasi Judul",
+				text: "Apakah anda yakin untuk memvalidasi tugas akhir ini?",
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Perbarui",
+				closeOnConfirm: false
+			},
+			function() {
+				$("#" + id)
+					.unbind("submit")
+					.submit();
+			}
+		);
+	});
+	$(".confirm").click(function(e) {
+		e.preventDefault();
+		var url = $(this).attr("href");
+		swal(
+			{
+				title: "Data Akan Dihapus",
+				text: "Apakah anda yakin untuk menghapus data ini?",
+				type: "warning",
+				showCancelButton: true,
+				confirmButtonColor: "#DD6B55",
+				confirmButtonText: "Hapus",
+				closeOnConfirm: false
+			},
+			function() {
+				window.location = url;
+			}
+		);
+	});
+});
